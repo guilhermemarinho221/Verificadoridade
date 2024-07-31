59d9e15e24b913ea0a99bfd85d38e2b6b5324178
Marinho <144070088+guilhermemarinho221@users.noreply.github.com>
2024-07-31 08:38:17 -0300

    Update script.js

--- ex015/script.js
+++ ex015/script.js
@@ -44,7 +44,7 @@ function verificar(){
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.<p>`
         res.appendChild(img)
     }
 

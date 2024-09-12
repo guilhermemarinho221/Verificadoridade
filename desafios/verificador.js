bcc513caebbd1bf0935c83be491025d1d7ab717d
Marinho <guigahunter2001@gmail.com>
2024-09-12 09:34:06 -0300

    manipulador de numeros

---
+++ desafios/verificador.js
@@ -0,0 +1,64 @@
+
+let num= document.getElementById("num")
+let ver =document.getElementById("lista")
+let res = document.getElementById("res")
+let valores = []
+function isnumero(n){
+    if(Number(n) >=1 && Number(n) <=100){
+        return true
+    }else{
+        return false
+    }
+
+}
+function inlista(n, l){
+    if (l.indexOf(Number(n))!= -1){
+        return true
+
+    } else {
+        return false
+    }
+
+}
+ function clicar(){
+    if(isnumero(num.value) && !inlista(num.value, valores) ){
+        valores.push (Number (num.value))
+        let item = document.createElement('option')
+        item.text =`Valor ${num.value} já encontrado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    }else{
+        window.alert('Valor invalido ou ja encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+
+ }
+ function finalizar() {
+    if (valores.length == 0 ) {
+        window.alert('Adicione valores antes de finalizar')
+
+    }else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores ){
+            soma += valores[pos]
+            if(valores[pos]> maior)
+                maior  = valores[pos]
+            if (valores[pos]< menor)
+                menor = valores [pos]
+        }
+        media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML +=`<p>Ao todo,temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor} </p>`
+        res.innerHTML +=  `<p> O maior valor informado foi ${maior} </p>`
+        res.innerHTML += `<p> somando todos os valores ${soma} </p>`
+        res.innerHTML += `<p> A média dos valores digitados é ${media} </p>`
+    }
+ }
+  
